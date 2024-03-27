b659ee6fe38c9e132759901c69b27c664b536bf5
Tiago Souza <tiagoeduardosouza@gmail.com>
2024-03-27 18:39:35 -0300

    Implementacao do login

--- src/main/java/com/sansasdev/garagem/entites/Cliente.java
+++ src/main/java/com/sansasdev/garagem/entites/Cliente.java
@@ -1,5 +1,12 @@
 package com.sansasdev.garagem.entites;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -12,7 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Cliente {
+public class Cliente implements UserDetails {
   private Long id;
 
   private String nome;
@@ -21,5 +28,41 @@ public class Cliente {
   private String email;
 
   private String password;
+
+  private ClienteRole role;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    if (this.role == ClienteRole.ADMIN) {
+      return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+    } else {
+      return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 
 }
